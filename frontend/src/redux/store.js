c70bdabb3375343763d525da2751c479bab351da
SANSKAR GUPTA <sanskarguptabaisali@gmail.com>
2024-04-23 13:10:59 +0530

    Upgrading the store to persistent storage

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,10 +1,25 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
 import { userReducer } from "./slice/userSlice";
 import { productReducer } from "./slice/productSlice";
 
-export const store = configureStore({
-  reducer: {
-    user: userReducer,
-    product: productReducer,
-  },
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  user: userReducer,
+  product: productReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  //   devTools: process.env.NODE_ENV !== "production",
+  //   middleware: [thunk],
+});
+
+export const persistor = persistStore(store);
